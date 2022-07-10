9a89b2068f59d0488333339910b4a9a86b40afbc
GabrielRodriguesMoreira <80790674+GabrielRodriguesMoreira@users.noreply.github.com>
2022-07-10 20:53:46 -0300

    iniciando projeto

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script setup>
+import ScrollBar from './components/ScrollBar.vue'
+</script>
+
+
+<template>
+  <header>
+  </header>
+
+  <main >
+    <ScrollBar/>
+  </main>
+
+</template>
+
+
+<style>
+body{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  background-image: url('https://img.freepik.com/premium-vector/ancient-egypt-egyptian-hieroglyphs-seamless-pattern-antique-elements-symbols-papyrus-historical-traditional-background-pyramids-graphic-decor-textile-wrapping-paper-wallpaper-vector-texture_176411-2756.jpg');
+    font-family: "myfont";
+}
+</style>
